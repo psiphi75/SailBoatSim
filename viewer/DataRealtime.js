7866532cdb895cdcc4a661e417f2b9ca4fcc3eef
Simon Werner <simonwerner@gmail.com>
2016-08-12 23:07:30 +1200

    Fixed realtime issues

--- viewer/DataRealtime.js
+++ viewer/DataRealtime.js
@@ -1,7 +1,8 @@
 
 function Realtime(url, channel, connectionCallback) {  // eslint-disable-line no-unused-vars
     'use strict';
-
+url = '192.168.43.18'
+channel = 'Perfectly Yummy Antitau'
     //
     // Connection stuff
     //
@@ -15,7 +16,6 @@ function Realtime(url, channel, connectionCallback) {  // eslint-disable-line no
     //
     // For AHRS code
     //
-    var hasGPS = false;
     var AHRS = require('ahrs');
     var madgwick = new AHRS({
         sampleInterval: 5,
@@ -30,7 +30,6 @@ function Realtime(url, channel, connectionCallback) {  // eslint-disable-line no
         observer.on('register', function () {
             console.log('Registered on channel: ' + observer.channel + ' with UID: ' + observer.uid);
             callCallback(true);
-            // clearTimeout(h);
             observer.on('status', handleBoatStatusUpdate);
         });
         observer.on('error', handleError);
@@ -60,51 +59,13 @@ function Realtime(url, channel, connectionCallback) {  // eslint-disable-line no
     }
 
     function handleBoatStatusUpdate(status) {
-
-        if (typeof status === 'object' && typeof status.compassRaw === 'object') {
-            boatStatus = getBoatStatusFromActualBoat(status);
-        } else if (status.isSimulation) {
-            boatStatus = status;
-        }
-
-    }
-
-    function getBoatStatusFromActualBoat(status) {
-        if (status.gps && status.gps.position) {
-            hasGPS = true;
-        }
-        if (!hasGPS) {
-            status.gps = {
-                // latitude: 0.001,
-                // longitude: 0.001,
-                latitude: -36.8095,
-                longitude: 174.7505,
+        if (!status.boat.gps) {
+            status.boat.gps = {
+                latitude: -36.85570036201125, longitude: 174.72794221011702,
                 altitude: 0
             };
         }
-
-        //
-        // Calculate attitude
-        //
-
-        var dt = status.time - lasttime;
-        lasttime = status.time;
-        madgwick.update(status.gyro.x * Math.PI / 180, status.gyro.y * Math.PI / 180, status.gyro.z * Math.PI / 180,
-            status.accel.x, status.accel.y, status.accel.z,
-            status.compassRaw.x, status.compassRaw.y, status.compassRaw.z, dt / 1000);
-
-        //
-        // Get the result of the updates
-        //
-
-        status.attitude = madgwick.getQuaternion();
-
-        var hpr = madgwick.getEulerAngles();
-        status.attitude.heading = hpr.heading * 180 / Math.PI;
-        status.attitude.pitch = hpr.pitch * 180 / Math.PI;
-        status.attitude.roll = hpr.roll * 180 / Math.PI;
-
-        return status;
+        boatStatus = status;
     }
 
 }
